ebd5c0cf189ac6b04d9ec8486bc4ac32e68aed62
hamso <hamsonassor14@gmail.com>
2023-06-23 14:37:27 +0300

    set up basic django project

--- car_rental/settings/base.py
+++ car_rental/settings/base.py
@@ -98,12 +98,6 @@ WSGI_APPLICATION = 'car_rental.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
