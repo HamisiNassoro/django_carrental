6c79bf7da671d3fd750917b4a0804de7644a7081
hamso <hamsonassor14@gmail.com>
2023-07-10 16:02:20 +0300

    profile module completed

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -57,6 +57,10 @@ class Profile(TimeStampedUUIDModel):
         default=False,
         help_text=_("Do you want to provide car rental services?"),
     )
+    is_agent = models.BooleanField(
+        verbose_name=_("Agent"), default=False, help_text=_("Are you an agent?")
+    )
+    top_agent = models.BooleanField(verbose_name=_("Top Agent"), default=False)
     rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     num_reviews = models.IntegerField(
         verbose_name=_("Number of Reviews"), default=0, null=True, blank=True
