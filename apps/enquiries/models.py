783eba6f886efd26e300bb12d9d42bc03ebb28ac
hamso <hamsonassor14@gmail.com>
2023-08-09 12:04:43 +0300

    added enquiries models and serializer

--- apps/enquiries/models.py
+++ apps/enquiries/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+from apps.common.models import TimeStampedUUIDModel
 
 # Create your models here.
+
+
+class Enquiry(TimeStampedUUIDModel):
+    name = models.CharField(_("Your Name"), max_length=100)
+    phone_number = PhoneNumberField(
+        _("Phone number"), max_length=30, default="+254712345678"
+    )
+    email = models.EmailField(_("Email"))
+    subject = models.CharField(_("Subject"), max_length=100)
+    message = models.TextField(_("Message"))
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name_plural = "Enquiries"
