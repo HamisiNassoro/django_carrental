c16dff9cf341ca906e0eb30106b0879f040cec4e
hamso <hamsonassor14@gmail.com>
2023-08-09 11:13:51 +0300

    updated the urls

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/profile/", include("apps.profiles.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) ### this is for local use if using docker service on NGINX can be commented out
 
 
