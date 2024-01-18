d85de81d8f4205c9df0e4eefc4a7f0f7737be3d2
Hamisi <hamsonassor14@gmail.com>
2024-01-18 14:42:44 +0300

    client src code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Car Rental</h1>
+      <p1>The Real test starts here...</p1>
+    </div>
+  );
+}
+
+export default App;
