ab5ebc7c75cfc5bf4b00ca10882f8de9b5288bf2
Hamisi <hamsonassor14@gmail.com>
2024-06-13 16:53:06 +0300

    setup nginx and react with docker

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Car Rental</h1>
+      <p>The real test starts here...</p>
+    </div>
+  );
+}
+
+export default App;
