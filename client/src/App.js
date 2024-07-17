f6bc9c92b1bf3b88fae9c5c24329f352b129d19d
Hamisi <hamsonassor14@gmail.com>
2024-07-17 15:04:01 +0300

    setup home and cars page, load data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import './App.css';
+import { Layout } from "antd";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import AppFooter from "./components/common/AppFooter";
+import AppHeader from "./components/common/AppHeader";
+import HomePage from "./pages/HomePage";
+import CarListPage from "./pages/CarListPage";
+
+const { Content, Header, Footer } = Layout;
 
 function App() {
-  return (
-    <div>
-      <h1>Car Rental</h1>
-      <p>The real test starts here...</p>
-    </div>
-  );
+	return (
+		<Router>
+			<Layout className="main-layout">
+				<Header>
+					<AppHeader />
+				</Header>
+				<Content>
+					<Route exact path="/" component={HomePage} />
+					<Route
+						exact
+						path="/cars"
+						component={CarListPage}
+					/>
+				</Content>
+				<Footer>
+					<AppFooter />
+				</Footer>
+			</Layout>
+		</Router>
+	);
 }
 
 export default App;
