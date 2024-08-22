10313e3f4df7256166ea2050975a2c416c5f8b78
Hamisi <hamsonassor14@gmail.com>
2024-08-22 15:24:44 +0300

    Not Found Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import NotFound from "./components/NotFound";
 import HomePage from "./pages/HomePage";
 import CarsPage from "./pages/CarsPage";
 
@@ -13,18 +15,17 @@ const App = () => {
 				<Header />
 				<main className="py-3">
 					<Routes>
-						<Route path="/" element={<HomePage />}></Route>
-					</Routes>
-					<Routes>
+						<Route path="/" element={<HomePage />} />
 						<Route
 							path="/cars"
 							element={<CarsPage />}
-						></Route>
+						/>
+						<Route path="*" element={<NotFound />} />
 					</Routes>
+					<ToastContainer theme="dark" />
 				</main>
 				<Footer />
 			</Router>
-			<ToastContainer />
 		</>
 	);
 };
